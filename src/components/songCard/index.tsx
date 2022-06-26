163f8c413e0305583c1ba2753288fd1664c00c37
adnanali8648 <adnan.ali8648@gmail.com>
2022-06-26 20:27:54 +0200

    song card

--- src/components/songCard/index.tsx
+++ src/components/songCard/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Card, LikeMe, MediaPlayer } from '../../UI';
+import { MediaPlayer, Card } from '../../UI';
 
-
-const SongCard: React.FC = () => {
+interface Props {
+  song: any;
+}
+const SongCard: React.FC<Props> = ({ song }) => {
   return (
     <Card>
       <div className="flex flex-col sm:flex-row sm:items-center  w-full h-full">
         <div className="w-full sm:w-1/4  bg-black">
-          <img className="w-full h-full" src="https://play-lh.googleusercontent.com/QovZ-E3Uxm4EvjacN-Cv1LnjEv-x5SqFFB5BbhGIwXI_KorjFhEHahRZcXFC6P40Xg" alt="" />
+          <img className="w-full h-full" src={song.cover_image_path} alt="" />
         </div>
         <div className=" h-full w-full sm:w-3/4 flex flex-col p-5">
-          <h2 className="font-bold text-3xl">testname</h2>
+          <h2 className="font-bold text-3xl">{song.name}</h2>
           <span className="font-normal text-sm text-slate-400">
-            by test singer
+            {`by ${song?.artist_name}`}
           </span>
-          <MediaPlayer />
-          <LikeMe />
+          <MediaPlayer
+            path={song.music_file_path}
+            type={song.music_file_mimetype}
+          />
         </div>
       </div>
     </Card>
--- src/components/songsList.tsx
+++ src/components/songsList.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useHttp from '../hooks/http';
+import { RequestMethod } from '../types';
 import SongCard from './songCard';
 
-
 const SongsList: React.FC = () => {
+  const { isLoading, data, error, sendRequest } = useHttp();
 
- 
+  useEffect(() => {
+    sendRequest('/song/trending', RequestMethod.GET);
+  }, [sendRequest]);
+
   return (
     <>
       <h1 className="font-bold text-5xl text-center text-white py-4">
         Songs list
       </h1>
-          <SongCard  />
+      {!isLoading && data && data.map((song: object, index: number) => (
+          <SongCard key={index} song={song} />
+        ))}
     </>
   );
 };
